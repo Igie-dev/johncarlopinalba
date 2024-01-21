90b7cddc56f20fbe8497de3039396b93110d4242
Igie-oss <baldesansoigieboysanchez@gmail.com>
2024-01-22 01:06:14 +0800

    change base for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
+	base: "https://igie-oss.github.io/johncarlopinalba/",
 	build: {
 		outDir: "dist",
 		sourcemap: true,
